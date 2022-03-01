4eb7235e940cf39e929af55ce2fc00ae2cf36727
Ivan Saez Zamora <ivsaez@gmail.com>
2022-03-01 18:35:25 +0100

    add enumerations

---
+++ src/enumerations.ts
@@ -0,0 +1,71 @@
+export enum AgeKind
+{
+    Kid,
+    Teenager,
+    Young,
+    Midage,
+    Old
+}
+
+export enum ComplexionKind
+{
+    Skinny,
+    Thin,
+    Fat,
+    Athletic,
+    Strongman
+}
+
+export enum EyeColor
+{
+    Black,
+    Brown,
+    Blue,
+    Green
+}
+
+export enum HairColor
+{
+    White,
+    Black,
+    Brown,
+    Red,
+    Blond,
+    Blue
+}
+
+export enum HaircutStyle
+{
+    Shaved,
+    Short,
+    StraightLong,
+    CurvyLong,
+    Bald
+}
+
+export enum OriginKind
+{
+    European,
+    African,
+    Asian,
+    American,
+    Arabic,
+    Oceanic
+}
+
+export enum SexKind
+{
+    Male,
+    Female,
+    Androgynous
+}
+
+export enum SpecieKind
+{
+    Human,
+    Dwarf,
+    Elf,
+    Goblin,
+    Orc,
+    Troll
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+export { 
+  AgeKind, 
+  EyeColor, 
+  ComplexionKind, 
+  HairColor, 
+  HaircutStyle, 
+  OriginKind, 
+  SexKind, 
+  SpecieKind 
+} from "./enumerations";
+
 function hello(name: string = "Ivan"): string {
     return `Hello, ${name}`;
   }
